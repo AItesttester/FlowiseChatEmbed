f52cbae46bf6015e62c38d1eedeccfaa9153765b
AItesttester <139743497+AItesttester@users.noreply.github.com>
2024-05-29 18:10:25 +0300

    Update TextInput.tsx changed input message placeholder
    Update TextInput.tsx changed input message placeholder

--- src/components/inputs/textInput/components/TextInput.tsx
+++ src/components/inputs/textInput/components/TextInput.tsx
@@ -93,7 +93,7 @@ export const TextInput = (props: Props) => {
         value={inputValue()}
         fontSize={props.fontSize}
         disabled={props.disabled}
-        placeholder={props.placeholder ?? 'Type your question'}
+        placeholder={props.placeholder ?? '...הקלד/י'}
       />
       {props.uploadsConfig?.isSpeechToTextEnabled ? (
         <RecordAudioButton
